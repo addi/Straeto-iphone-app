5695121c2120e0651ec126634f086864797b9a65
Árni Jónsson <addi@vanilla.local>
2012-09-19 13:03:34 +0000

    status bar fix

--- Classes/ScheduleViewController.m
+++ Classes/ScheduleViewController.m
@@ -59,9 +59,7 @@
 - (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
 {
     if (shouldGetSchedule)
-    {
-        NSLog(@"new location: %@", newLocation);
-        
+    {        
         [self fetchSchedule:newLocation];
         
         shouldGetSchedule = NO;
@@ -85,8 +83,6 @@
     
     [request setCompletionBlock:^{
         NSString *responseString = [request responseString];
-                
-//        NSLog(@"response: %@", responseString);
         
         [self parseSchduleData:responseString];
     }];
@@ -127,8 +123,6 @@
         
         lastStopId = stopId;
     }
-    
-    NSLog(@"done");
     
     [self.tableView reloadData];
 }
@@ -191,10 +185,6 @@
     
     if (cell == nil)
     {
-//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-//
-//        cell.userInteractionEnabled = NO;
-        
         NSArray *nib = [[NSBundle mainBundle] loadNibNamed:CellNib owner:self options:nil];
         
         cell = (UITableViewCell *)[nib objectAtIndex:0];
@@ -215,59 +205,6 @@
     time2Lable.text = [[route valueForKey:@"times"] objectAtIndex:1];
 
     return cell;
-}
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     [detailViewController release];
-     */
 }
 
 @end
